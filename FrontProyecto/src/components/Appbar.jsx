9434e7592f80bf6b6fa1cacb33685576e7b14da5
LitaHuanca <lita.huanca@unmsm.edu.pe>
2024-10-26 15:29:38 -0500

    Entendiendo2?

--- FrontProyecto/src/components/Appbar.jsx
+++ FrontProyecto/src/components/Appbar.jsx
@@ -47,7 +47,7 @@ function ResponsiveAppBar() {
   };
 
   return (
-    <AppBar position="static" sx={{bgcolor:"cyan"}}>
+    <AppBar position="static" sx={{bgcolor:"pink"}}>
       <Container maxWidth="xl">
         <Toolbar disableGutters>
           <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
