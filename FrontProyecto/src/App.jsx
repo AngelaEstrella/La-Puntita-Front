3ddf10b559e18814f44f60b53057423eb9f592e6
NelsonRamos1512 <nelson.ramosa@unmsm.edu.pe>
2024-10-27 14:59:23 -0500

    "Appbar.jsx/app change"

--- FrontProyecto/src/App.jsx
+++ FrontProyecto/src/App.jsx
@@ -3,6 +3,7 @@ import ResponsiveAppBar from './components/Appbar';
 import Inicio from './pages/Home/Home';
 import Login from './components/Login';
 import Carta from './pages/Carta/Carta';
+
 export default function App() {
   return (
     <Router>
@@ -18,6 +19,9 @@ export default function App() {
                   <Route path="/" element={<Inicio />} />
                   <Route path="/carta" element={<Carta />} />
                   <Route path="/ventas" element={<h1>Ventas</h1>} />
+                  {/* Rutas para Delivery y Recojo en tienda */}
+                  <Route path="/delivery" element={<h1>Delivery</h1>} />
+                  <Route path="/pickup" element={<h1>Recojo en tienda</h1>} />
                 </Routes>
               </>
             }
