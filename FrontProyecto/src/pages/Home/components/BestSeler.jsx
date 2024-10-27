c3199adebc0b675eff27f25988bd8a82e18743d3
NelsonRamos1512 <nelson.ramosa@unmsm.edu.pe>
2024-10-27 12:07:15 -0500

    "Cambios en el diseño"

--- FrontProyecto/src/pages/Home/components/BestSeler.jsx
+++ FrontProyecto/src/pages/Home/components/BestSeler.jsx
@@ -1,24 +1,20 @@
-import Typography from '@mui/material/Typography'
-import "./BestSeler.css"
+import Typography from '@mui/material/Typography';
+import "./BestSeler.css";
 
-const pikachu = "https://static.vecteezy.com/system/resources/previews/024/804/557/non_2x/pikachu-art-or-illustration-on-pickachu-free-vector.jpg"
-let array = [pikachu,pikachu,pikachu,pikachu]
+const pikachu = "https://static.vecteezy.com/system/resources/previews/024/804/557/non_2x/pikachu-art-or-illustration-on-pickachu-free-vector.jpg";
+const productosPopulares = [pikachu, pikachu, pikachu, pikachu]; // Array de productos populares
 
-export default function BestSeler(){
-    return(
+export default function BestSeler() {
+    return (
         <>
-        <Typography
-                variant="h4"
-            >
-                Mas Vendidos
+            <Typography variant="h4" sx={{ textAlign: 'center', marginBottom: '20px' }}>
+                Más Vendidos
             </Typography>
-        <div className="container">
-            {array.map(image =>{
-                return(
-                    <img className="best-images" src = {pikachu}/>
-                )
-            } )}
-        </div>
+            <div className="container">
+                {productosPopulares.map((image, index) => (
+                    <img key={index} className="best-images" src={image} alt={`Producto popular ${index + 1}`} />
+                ))}
+            </div>
         </>
-    )
+    );
 }
