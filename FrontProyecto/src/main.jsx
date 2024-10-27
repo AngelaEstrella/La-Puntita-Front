efa0d94effed4cc5019c9028de6adb6c413aa57f
SubDSR <david.sevan@unmsm.edu.pe>
2024-10-27 16:02:23 -0500

    borranding noma

--- FrontProyecto/src/main.jsx
+++ FrontProyecto/src/main.jsx
@@ -2,12 +2,10 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './main.css'; // Importamos el archivo CSS global para aplicar estilos
-import PromocionBebidas from './components/PromocionBebidas';
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <PromocionBebidas />
   </StrictMode>
 );
