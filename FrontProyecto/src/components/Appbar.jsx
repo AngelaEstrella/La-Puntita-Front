4c8987b2f2962e4ad7ce1ab3aa95b93b2ba20869
SubDSR <david.sevan@unmsm.edu.pe>
2024-10-27 15:32:21 -0500

    mejor no probe nada

--- FrontProyecto/src/components/Appbar.jsx
+++ FrontProyecto/src/components/Appbar.jsx
@@ -116,7 +116,7 @@ function ResponsiveAppBar() {
             
           {/* Botones de Delivery y Recojo en Tienda */}
           <Box sx={{ display: 'flex', gap: 2, marginRight: '20px' }}>
-            {/*
+            
             <Button
               variant="contained"
               color="secondary"
@@ -132,16 +132,7 @@ function ResponsiveAppBar() {
             >
               Recojo en Tienda
             </Button>
-            */}
-
-            <Button
-              variant="contained"
-              startIcon={<LocationOnIcon />}
-              color="secondary"
-              classname="deliveryButton"
-            >
-              ¿Cómo deseas recibir tu pedido?
-            </Button>
+            
 
             <Button
               variant="contained"
