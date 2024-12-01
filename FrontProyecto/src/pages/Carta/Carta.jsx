971c32b819c0191a18b8994d05ceb6f6ac34ff65
litaPARK <litaht2004@gmail.com>
2024-12-01 15:49:09 -0500

    CambioImágenesCatálogo

--- FrontProyecto/src/pages/Carta/Carta.jsx
+++ FrontProyecto/src/pages/Carta/Carta.jsx
@@ -10,23 +10,6 @@ const categorias = {
     Maxipizzas: ["Moreno", "Gringo", "Vergano", "Juguetón", "Carnoso"]
 };
 
-const urlsImagenes = {
-    "Latino": "https://i.ibb.co/DzFDp4L/ElLatino.png",
-    "Suggar daddy": "https://i.ibb.co/crWVJCV/Suggar-Daddy.png",
-    "Europeo": "https://i.ibb.co/qNgVkhG/Europeoo.png",
-    "Africano": "https://i.ibb.co/6w5LxGb/El-Africano.png",
-    "Dinamita": "https://i.ibb.co/02tjjs4/La-Dinamita.png",
-    "Cocolover": "https://i.ibb.co/t8BqjTS/Coco-Lover.png",
-    "Europea": "https://i.ibb.co/9bdb6yK/Europea.png",
-    "Pichanguera": "https://i.ibb.co/0yfZRnZ/Pichangera.png",
-    "Power": "https://i.ibb.co/VYYkYvk/Power.png",
-    "Malcriada": "https://i.ibb.co/jzDF80x/Malcriada.png",
-    "Moreno": "https://i.ibb.co/hY7WDqX/Moreno.png",
-    "Gringo": "https://i.ibb.co/19g2pmJ/Gringo.png",
-    "Vergano": "https://i.ibb.co/qrqw4rC/Vergano.png",
-    "Juguetón": "https://i.ibb.co/kHYSW1B/Jugueton.png",
-    "Carnoso": "https://i.ibb.co/wykQF1T/Carnoso.png"
-};
 
 export default function Carta() {
     const [productos, setProductos] = useState([]);
@@ -76,10 +59,10 @@ export default function Carta() {
                 <div className="producto-lista">
                     {filteredProducts.map((product) => (
                         <div key={product.idProducto} className="producto-tarjeta">
-                            <img src={urlsImagenes[product.nombreProducto]} alt={product.nombreProducto} className="producto-imagen" />
+                            <img src={product.imagen} alt={product.nombreProducto} className="producto-imagen" />
                             <div className="producto-info">
                                 <h3 className="producto-nombre">{product.nombreProducto}</h3>
-                                <p className="producto-descripcion">{product.descripcion}</p>
+                                <p className="producto-descripcion">{product.descripcion || "Sin descripción"}</p>
                                 <p className="producto-precio">S/ {product.precioUnitario}</p>
                                 <button className="boton-agregar" onClick={() => setSelectedProduct(product)}>Agregar al pedido</button>
                             </div>
